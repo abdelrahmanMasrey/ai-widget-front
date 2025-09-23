d6b3cb17a7e54d2726f709cd5adff9c2d40e13a8
abdelrahmanMasrey <abdelrahman.masrey@gmail.com>
2025-09-23 19:43:17 +0200

    svs

--- src/callWidget.jsx
+++ src/callWidget.jsx
@@ -198,11 +198,7 @@ function AICallWidget({ position = "bottom-right", agentId, color, text }) {
           aria-label="Toggle AI Assistant"
           style={{ backgroundColor: color }}
         >
-           <img 
-    src="emoji.png" 
-    alt="AI Assistant" 
-    style={{ width: "45px", height: "45px" }} 
-  />
+            <img src="/assets/emoji.png" alt="AI Assistant" style={{ width: "45px", height: "45px" }} />
         </button>
       </div>
 
